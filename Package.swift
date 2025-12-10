5c9a5b020cce28952ee9491e2a295c5fa7c6e1c9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:30:25 +0100

    Update dependencies to latest versions
    - Update swift-incits-4-1986 from 0.3.0 to 0.6.2
    - Add swift-standards from 0.10.0 (required for Binary module)

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         .library(name: .rfc6750, targets: [.rfc6750]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.3.0"),
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.2"),
     ],
     targets: [
         .target(
